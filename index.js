bcbcc7335f1abea0fd207aeb1e88292a060e0282
Danieleclima <daniel.echuaca.lima@gmail.com>
2021-07-16 16:30:32 +0100

    creating send ads function

--- index.js
+++ index.js
@@ -12,18 +12,32 @@ const { google } = require("googleapis");
   const content = await page.content();
   let dom = new JSDOM (content)
   Array.from(dom.window.document.querySelectorAll("[data-text-ad]")).map( ad =>
-    console.log(ad.textContent) )
+    console.log(ad.textContent))
   // console.log(content.includes("data-text-ad"));
 
   await browser.close();
 })();
 
-(async () => {
+async function SendAds (ads) {
 
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
-  
-})
-// console.log(dom)
+
+    // Instance of Google Sheets API
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+
+    const spreadsheetId = "1eQpKSozTwm1KhpexY8rNTweSeTscADqbCsSKTdHIUks";
+
+    await googleSheets.spreadsheets.values.append({
+      auth,
+      spreadsheetId,
+      range: "Sheet1",
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values: [[]],
+      },
+    })
+
+}
