77f92ca41272a420264217a0c629b4f30a38dea3
Danieleclima <daniel.echuaca.lima@gmail.com>
2021-07-17 18:52:38 +0100

    aggregate ads function created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { firefox } = require('playwright');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const { google } = require("googleapis");
-const keyword = "white+shoes";
+const keyword = "red+shoes";
 const today = new Date();
 const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+ ' ' + today.getHours() + ":" + today.getMinutes();
 
@@ -15,15 +15,16 @@ const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+ '
   await page.goto(`https://www.google.com/search?q=${keyword}`);
   const content = await page.content();
   let dom = new JSDOM (content)
-  let textAds = aggregateAds(dom)
-  sendAds(textAds);
+  let Ads = aggregateAds(dom)
+  sendAds(Ads);
   await browser.close();
 })();
 
 // fetching all text ads from the DOM
 function selectTextAds (domObject) {
    let allTextAds = []
-  // iterating over an ad list
+   let type = "Text Ad";
+  // iterating over a text ads list
   if (domObject.window.document.querySelector("*").outerHTML.includes("data-text-ad")){
   domObject.window.document.querySelectorAll("[data-text-ad]").forEach( (ad, index) =>{
     let position = index + 1;
@@ -35,7 +36,6 @@ function selectTextAds (domObject) {
       description = ad.textContent
       }
     let path = ad.querySelector("span:nth-child(2)").textContent;
-    let type = "Text Ad";
     let newTextAd = [date, keyword, position, headline, description, path, type];
     allTextAds.push(newTextAd);
   })
@@ -50,6 +50,7 @@ return allTextAds
 // fetching all Shopping ads from the DOM
 function selectShoppingAds (domObject) {
   let allShoppingAds = []
+  let type = "Shopping Ad"
    if (domObject.window.document.querySelector("*").outerHTML.includes("top-pla-group-inner")) {
      let list = Array.from(domObject.window.document.querySelectorAll(".pla-unit-container"))
      list.pop();
@@ -57,20 +58,26 @@ function selectShoppingAds (domObject) {
       let position = index + 1;
       let headline = ad.querySelector(".pla-unit-title").textContent
       let advertiser = ad.querySelectorAll("span")[2].textContent
-      let type = "Shopping Ad"
       let newShoppingAd = [date, keyword, position, headline, "N/A", advertiser, type]
       allShoppingAds.push(newShoppingAd)
     })
   } else {
     let noAds = [date, keyword, "N/A", "N/A", "N/A", "N/A", type]
-    allTextAds.push(noAds)
+    allShoppingAds.push(noAds)
 }
+// console.log(allShoppingAds)
+return allShoppingAds
 }
 
 function aggregateAds (domObject) {
-  let arrayofAds = selectTextAds(domObject).concat(selectShoppingAds(domObject))
-  console.log(arrayofAds.flat())
-  return arrayofAds.flat()
+  let allAds = selectTextAds(domObject);
+  console.log(allAds)
+  let shoppingAds = selectShoppingAds(domObject);
+  shoppingAds.forEach(element => {
+    allAds.push(element)
+  });
+  // console.log(allAds);
+  return allAds
 }
 
 async function sendAds (ads) {
